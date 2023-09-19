9382f00b999e815763206b086fb84ab706872a63
Shruti Mehrotra <msshruti@amazon.co.uk>
2023-09-19 18:26:30 +0100

    Revert "Consumer and producer should wait for kafkaManager to create topics"
    This reverts commit ae242163461898425b9a58bcc3cf7ec3bf382f6a.

--- src/main/java/com/harness/ProduceToKafka.java
+++ src/main/java/com/harness/ProduceToKafka.java
@@ -24,7 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Controller;
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
@@ -105,7 +104,6 @@ public class ProduceToKafka {
     private Properties props;
     private AmazonCloudWatch cwClient;
 
-    @DependsOn("KafkaManager.class")
     @PostConstruct
     void initAndStart() {
         if (startProducer) {
